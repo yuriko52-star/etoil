7b39f717435c72323c7fb08a7c91f7afee5bd5ef
yuriko <yuriko40223@gmail.com>
2024-10-19 08:03:12 +0900

    5回目のコミット　更新削除機能を実装

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -41,9 +41,12 @@
             @foreach($todos as $todo)
             <tr class="todo-table__row">
                 <td class="todo-table__item">
-                    <form action="" class="update-form" method="">
+                    <form action="/todos/update" class="update-form" method="post">
+                        @method('PATCH')
+                        @csrf
                         <div class="update-form__item">
-                            <input type="text" class="update-form__item-input"value="{{ $todo['content'] }}">
+                            <input type="text" class="update-form__item-input" name="content" value="{{ $todo['content'] }}">
+                            <input type="hidden" name="id" value="{{ $todo['id'] }}">
                         </div>
                         <div class="update-form__button">
                             <button class="update-form__button-submit"type="submit">更新</button>
@@ -51,8 +54,11 @@
                     </form>
                 </td>
                 <td class="todo-table__item">
-                    <form action="" class="delete-form">
+                    <form action="/todos/delete" class="delete-form" method="post">
+                        @method('DELETE')
+                        @csrf
                         <div class="delete-form__button">
+                            <input type="hidden" name="id" value="{{ $todo['id'] }}">
                             <button class="delete-form__button-submit"type="submit">削除
 
                             </button>
--- src/routes/web.php
+++ src/routes/web.php
@@ -19,3 +19,5 @@ Route::get('/', function () {
 });
 Route::get('/',[TodoController::class,'index']);
 Route::post('/todos',[TodoController::class,'store']);
+Route::patch('/todos/update',[TodoController::class,'update']);
+Route::delete('/todos/delete',[TodoController::class,'destroy']);
