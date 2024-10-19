aa80a57773274fc8e8ac9576552e1b83f66879cb
yuriko <yuriko40223@gmail.com>
2024-10-19 11:17:44 +0900

    6回目のコミット　テーブル作成

--- src/app/Models/Todo.php
+++ src/app/Models/Todo.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
     use HasFactory;
-    protected $fillable = ['content'];
+    protected $fillable = ['category_id','content'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
